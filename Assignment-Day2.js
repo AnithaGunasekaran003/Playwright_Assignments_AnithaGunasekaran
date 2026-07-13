e923f35bdf85b7c9498039355ca37f40f5f5a6f7
AnithaGunasekaran003 <66992819+AnithaGunasekaran003@users.noreply.github.com>
2026-07-13 15:36:21 +0530

    Add files via upload
    Week1-Day2 Assignment

---
+++ Assignment-Day2.js
@@ -0,0 +1,19 @@
+
+let userName = "Anitha"
+console.log(userName);
+console.log(typeof userName);
+
+let organization = "Google"
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber = 123123
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation = true
+console.log(knowsAutomation);
+
+let undefined
+console.log(undefined);
+
